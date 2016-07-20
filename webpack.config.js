374105c001b0b1b33bc9c39cd14c693ee4018297
nurulc <nurulc@yahoo.com>
2016-07-20 17:47:39 -0400

    missed webpack config

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var path = require('path');
+//var webpack = require('webpack');
+
+
+module.exports = {
+	devtool: 'eval',
+    entry:  './src',
+    output: {
+        path:     path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                test:   /\.js$/,
+                loaders: ['babel-loader'],
+                exclude: path.join(__dirname , 'src/test'),
+                include: path.join(__dirname , 'src'),
+          
+            }
+        ],
+    }
+};
+
+/*
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+                  compress: {
+                      warnings: false
+                  },
+                  output: {comments: false}
+              })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.css$/, // Only .css files
+      loader: 'style!css' // Run both loaders
+    }]
+  }
+};
+
+babel-loader
+*/
