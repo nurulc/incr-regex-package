9f1da09ad0d9d51e2a0dbcc8a86419bd1bc14e86
nurulc <nurulc@yahoo.com>
2019-12-01 20:25:15 -0500

    upgrading node and babel

--- src/rxtree.js
+++ src/rxtree.js
@@ -1,5 +1,22 @@
 //rxtree.js
-//import {printExpr} from "./rxprint"; 
+//import {printExpr} from "./rxprint";
+/** 
+ * Copyright (c) 2016, Nurul Choudhury
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ */
+ 
 "use strict";
 import {StackDedup} from "./utils";
 //import {getArrayMask}  from "./incr-regex-v3"; // debugging only
